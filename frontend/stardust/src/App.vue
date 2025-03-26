aaf67645847e6340cd1bd9103e965c6271dc0898
paketoff <kekisreal228@gmail.com>
2025-03-26 11:38:11 +0100

    (UC-3-frontend): Init tailwind

--- frontend/stardust/src/App.vue
+++ frontend/stardust/src/App.vue
@@ -4,6 +4,8 @@
   <header>
     <div class="wrapper">
       <nav></nav>
+      <!-- just test to see if tailwind works so far -->
+      <h1 class="font-(family-name:Roboto) text-xl">Hello wrld!</h1>
     </div>
   </header>
 </template>
